df9f7e4c91bbe9517acc41e32d757f3bd56dc7a6
Aleksandr19May <dela737@mail.ru>
2023-04-09 12:06:21 +0400

    start

--- lib/features/ticket_storage/ticket_storage_page.dart
+++ lib/features/ticket_storage/ticket_storage_page.dart
@@ -6,6 +6,70 @@ class TicketStoragePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const FlutterLogo();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.grey.shade200,
+        title: Text(
+          'Хранение билетов',
+          style: TextStyle(color: Colors.black),
+        ),
+      ),
+      body: ListView.builder(
+        itemCount: 10,
+        itemBuilder:(BuildContext context, int index) {
+          return ListTile(
+            leading: Icon(Icons.train),
+            trailing: Icon(Icons.cloud_download_rounded,color: Colors.purple,),
+            title: Text('df'),
+          );
+        },
+    ),
+  bottomNavigationBar: Padding(
+    padding: const EdgeInsets.all(15.0),
+    child: Row(mainAxisAlignment: MainAxisAlignment.end,
+      children: [
+        ElevatedButton(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: Colors.blue.shade100,
+    shape: RoundedRectangleBorder(
+      borderRadius: BorderRadius.circular(15), 
+    ),
+  ), 
+  
+          onPressed: (){
+
+ showModalBottomSheet<void>(
+            context: context,
+            builder: (BuildContext context) {
+              return SizedBox(
+                height: 200,
+                child: Center(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    mainAxisSize: MainAxisSize.min,
+                    children: <Widget>[
+                      const Text('Modal BottomSheet'),
+                      ElevatedButton(
+                        child: const Text('Close BottomSheet'),
+                        onPressed: () => Navigator.pop(context),
+                      ),
+                    ],
+                  ),
+                ),
+              );
+            },
+          );
+
+
+
+
+
+          }, child: Text('Добавить', style: TextStyle(color: Colors.blue.shade900,fontSize: 17,fontWeight: FontWeight.w600),),),
+      ],
+    ),
+  ),
+    
+    );
+    
   }
 }
